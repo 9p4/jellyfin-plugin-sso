f4b318189e9496fae53453e0a583029bd22fcec5
Sambhav Saggi <17993169+sambhavsaggi@users.noreply.github.com>
2022-01-10 19:00:00 -0500

    Add more boilerplate

--- SSO-Auth/SSOAuthenticationProviderPlugin.cs
+++ SSO-Auth/SSOAuthenticationProviderPlugin.cs
@@ -3,6 +3,17 @@ using Jellyfin.Data.Entities;
 using Jellyfin.Data.Enums;
 
 namespace Jellyfin.Plugin.SSO_Auth {
-    public class LdapAuthenticationProviderPlugin : IAuthenticationProvider {
+    public class SSOAuthenticationProviderPlugin {
+      private readonly ILogger<SSOAuthenticationProviderPlugin> _logger;
+      private readonly IApplicationHost _applicationHost;
+
+      public SSOAuthenticationProviderPlugin(IApplicationHost applicationHost, ILogger<SSOAuthenticationProviderPlugin> logger) {
+        _logger = logger;
+        _applicationHost = applicationHost;
+      }
+
+      public string Name => "SSO-Authentication";
+
+      public bool isEnabled => true;
     }
 }
--- SSO-Auth/SSOPlugin.cs
+++ SSO-Auth/SSOPlugin.cs
@@ -6,4 +6,25 @@ using MediaBrowser.Common.Plugins;
 using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Serialization;
 
-namespace Jellyfin.Plugin.SSO_Auth {}
+namespace Jellyfin.Plugin.SSO_Auth {
+  public class SSOPlugin : BasePlugin<PluginConfiguration>, IHasWebPages {
+    public SSOPlugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer) : base(applicationPaths, xmlSerializer) {
+      Instance = this;
+    }
+
+    public static SSOPlugin Instance { get; private set; }
+
+    public override string Name => "SSO-Auth";
+
+    public override Guid Id => Guid.Parse("505ce9d1-d916-42fa-86ca-673ef241d7df");
+
+    public IEnumerable<PluginPageInfo> GetPages() {
+      return new[] {
+        new PluginPageInfo {
+          Name = Name,
+          EmbeddedResourcePath = $"{GetType().Namespace}.Config.configPage.html"
+        }
+      };
+    }
+  }
+}
