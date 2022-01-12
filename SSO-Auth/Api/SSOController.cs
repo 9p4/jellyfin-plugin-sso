9911791047f60cb1a02b889974f056c5cb12f602
Sambhav Saggi <17993169+sambhavsaggi@users.noreply.github.com>
2022-01-11 21:16:29 -0500

    Basic API, config pages, etc

--- SSO-Auth/Api/SSOController.cs
+++ SSO-Auth/Api/SSOController.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Net.Mime;
-using MediaBrowser.Common;
-using MediaBrowser.Controller.Authentication;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,17 +8,36 @@ namespace Jellyfin.Plugin.SSO_Auth.Api {
     /// The sso api controller.
     /// </summary>
     [ApiController]
-    [Authorize(Policy = "RequiresElevation")]
     [Route("[controller]")]
-    [Produces(MediaTypeNames.Application.Json)]
     public class SSOController : ControllerBase {
       [HttpPost("SAMLPost")]
       [ProducesResponseType(StatusCodes.Status200OK)]
       [ProducesResponseType(StatusCodes.Status400BadRequest)]
-      public IActionResult SAMLPost(string response) {
+      public IActionResult SAMLPost() {
+        StreamReader reader = new StreamReader(Request.Body);
+        string text = reader.ReadToEndAsync().Result;
         var configuration  = SSOPlugin.Instance.Configuration;
-        Saml.Response samlResponse = new Saml.Response(configuration.SamlCertificate, response);
+        Saml.Response samlResponse = new Saml.Response(configuration.SamlCertificate, text);
         return Ok(samlResponse);
+      }
+
+      [HttpGet("test")]
+      [ProducesResponseType(StatusCodes.Status200OK)]
+      public string[] test() {
+        return new string[] {
+          SSOPlugin.Instance.Configuration.SamlCertificate,
+            SSOPlugin.Instance.Configuration.SamlEndpoint
+        };
+      }
+
+      // https://stackoverflow.com/a/17535912
+      [HttpPost("testPost")]
+      [ProducesResponseType(StatusCodes.Status200OK)]
+      [Produces(MediaTypeNames.Application.Json)]
+      public string testPost() {
+        StreamReader reader = new StreamReader(Request.Body);
+        string text = reader.ReadToEndAsync().Result;
+        return text;
       }
     }
 }
