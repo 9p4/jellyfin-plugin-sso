0dbdb6c5463feaed4c7709e9942770adbe28bb11
Esmond <esmondmissen@users.noreply.github.com>
2023-01-01 12:11:31 -0500

    Update SSO-Auth/Api/SSOController.cs
    Co-authored-by: Balazs Benics <benicsbalazs@gmail.com>

--- SSO-Auth/Api/SSOController.cs
+++ SSO-Auth/Api/SSOController.cs
@@ -122,7 +122,7 @@ public class SSOController : ControllerBase
                 // Role processing
                 // The regex matches any "." not preceded by a "\": a.b.c will be split into a, b, and c, but a.b\.c will be split into a, b.c (after processing the escaped dots)
                 // We have to first process the RoleClaim string
-                string[] segments = string.IsNullOrEmpty(config.RoleClaim) ? Array.Empty<string>() : Regex.Split(config.RoleClaim?.Trim(), "(?<!\\\\)\\.");
+                string[] segments = string.IsNullOrEmpty(config.RoleClaim) ? Array.Empty<string>() : Regex.Split(config.RoleClaim.Trim(), "(?<!\\\\)\\.");
 
                 if (segments.Any())
                 {
